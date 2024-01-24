061201a8c8177839608a39dcec9b8a2f96bcb9f8
Braxton Fair <hello@taxborn.com>
2024-01-23 22:37:35 -0600

    add sign in functionality and enforce authentication everywhere

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main className="container mx-auto">
+    <main className="container mx-auto h-screen">
+      <UserButton afterSignOutUrl="/" />
+
       <h1 className="text-2xl font-bold">Hockey Management System</h1>
     </main>
   )
