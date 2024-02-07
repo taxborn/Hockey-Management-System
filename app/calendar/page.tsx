4bcf6b2c01a95d74f14cf7bf301ca5beee75756a
Braxton Fair <hello@taxborn.com>
2024-02-07 00:26:53 -0600

    create barebones calendar page and link to it

---
+++ app/calendar/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <main className="container mx-auto h-screen py-8">
+            <h1 className="text-2xl font-bold">Team Calendar</h1>
+        </main>
+    )
+}
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,6 +6,7 @@ export default async function Home() {
   return (
     <main className="container mx-auto h-screen py-8">
       <h1 className="text-2xl font-bold">Hello, {user?.firstName}</h1>
+      <a href="/calendar" className="font-semibold underline text-indigo-500">calendar</a>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nulla et cum adipisci. Quasi expedita dolore blanditiis molestias excepturi cupiditate, consectetur numquam architecto error ipsum perspiciatis, itaque iusto dicta reprehenderit.</p>
     </main>
   )
