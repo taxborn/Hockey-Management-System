619277d8d25cf1b00e02bd7afe9ea6fd1c7a8af8
Braxton Fair <hello@taxborn.com>
2024-02-07 00:21:08 -0600

    create custom sign-in and sign-up pages

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+import { currentUser } from "@clerk/nextjs";
+
+export default async function Home() {
+  const user = await currentUser();
+
+  return (
+    <main className="container mx-auto h-screen py-8">
+      <h1 className="text-2xl font-bold">Hello, {user?.firstName}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nulla et cum adipisci. Quasi expedita dolore blanditiis molestias excepturi cupiditate, consectetur numquam architecto error ipsum perspiciatis, itaque iusto dicta reprehenderit.</p>
+    </main>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main className="container mx-auto h-screen py-8">
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, officia minus perferendis itaque consectetur recusandae labore nulla ratione voluptas ipsum hic. Voluptatem itaque veritatis repellendus vel totam animi accusantium sunt.</p>
+      <h1 className="text-2xl font-bold">Goal Guardian</h1>
+      <p>Goal Guardian is a team-management application...</p>
     </main>
   )
 }
