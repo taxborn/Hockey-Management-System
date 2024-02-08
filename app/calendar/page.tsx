67a4b47ae152c4121aa806e2d20534d22ea24005
Braxton Fair <hello@taxborn.com>
2024-02-08 16:21:42 -0600

    add example dates

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -4,11 +4,15 @@ import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import { EventContentArg } from '@fullcalendar/core/index.js';
+import { EventContentArg, EventInput } from '@fullcalendar/core/index.js';
 
-const events = [
-    { title: 'event 1', date: '2024-02-07', end: '2024-02-09' },
-    { title: 'event 2', date: new Date() },
+const events: EventInput[] = [
+    { title: 'vs. Ohio State', date: new Date("2024-02-09 18:00:00"), extendedProps: {"homeGame": true} },
+    { title: 'vs. Ohio State', date: new Date("2024-02-10 14:00:00"), extendedProps: {"homeGame": true} },
+    { title: 'vs. Bemidji', date: new Date("2024-02-16 18:00:00"), extendedProps: {"homeGame": true} },
+    { title: 'vs. Bemidji', date: new Date("2024-02-17 15:00:00"), extendedProps: {"homeGame": true} },
+    { title: 'vs. St. Thomas', date: new Date("2024-02-23 15:00:00"), extendedProps: {"homeGame": true} },
+    { title: 'at St. Thomas', date: new Date("2024-02-24 14:00:00"), extendedProps: {"homeGame": false} },
 ]
 
 export default function Page() {
@@ -28,10 +32,12 @@ export default function Page() {
 }
 
 function renderEventContent(eventInfo: EventContentArg) {
+    const isHome = eventInfo.event.extendedProps.homeGame;
+
     return (
-        <>
+        <div className={isHome ? 'bg-indigo-200' : 'bg-amber-200'}>
             <b className='mr-2'>{eventInfo.timeText}</b>
-            <i className='font-bold'>{eventInfo.event.title}</i>
-        </>
+            <p className='inline-block'>{eventInfo.event.title}</p>
+        </div>
     )
 }
