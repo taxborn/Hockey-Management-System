af4f429cbeff5527190104a2c26814d80748ee31
Braxton Fair <hello@taxborn.com>
2024-02-08 12:15:59 -0600

    add sign in and sign up links

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,13 +1,22 @@
+"use client";
+
+import { useAuth } from "@clerk/nextjs";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Navbar() {
+  const { userId } = useAuth();
+
   return (
     <nav className="p-4 bg-indigo-200 w-full">
       <div className="flex justify-between max-w-screen-xl mx-auto">
-        <h2 className="text-xl font-bold tracking-tight">Goal Guardian</h2>
+        {/* TODO: If signed in, redirect to /home, otherwise redirect to / */}
+        <a href={userId ? '/home' : '/'} className="text-xl font-bold tracking-tight">Goal Guardian</a>
 
         {/* TODO: Replace the given <UserButton> Clerk button with something more light-weight */}
-        <UserButton afterSignOutUrl="/" />
+        {userId ? <UserButton afterSignOutUrl="/" /> : <div>
+          <a href="/sign-in" className="mr-2">sign in</a>
+          <a href="/sign-up">sign up</a>
+        </div>}
       </div>
     </nav>
   )
