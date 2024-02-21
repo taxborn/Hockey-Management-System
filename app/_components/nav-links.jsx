915832bd0b774ea37db9e7c8f1654dfdc4447878
Ruth Taye <rutheskinder92919@gmail.com>
2024-02-21 10:55:45 -0600

    added link\next so it won't do full page refresh when navigating

--- app/_components/nav-links.jsx
+++ app/_components/nav-links.jsx
@@ -6,6 +6,8 @@ import {
     ClipboardIcon,
     ChatBubbleBottomCenterTextIcon
   } from '@heroicons/react/24/outline';
+
+  import Link from 'next/link';
   
 
   const links = [
@@ -27,14 +29,14 @@ import {
         {links.map((link) => {
           const LinkIcon = link.icon;
           return (
-            <a
+            <Link
               key={link.name}
               href={link.href}
               className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
             >
               <LinkIcon className="w-6" />
               <p className="hidden md:block">{link.name}</p>
-            </a>
+            </Link>
           );
         })}
       </>
