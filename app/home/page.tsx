7d845567b64c46ea7604d91b0c5c985393af659c
Braxton Fair <hello@taxborn.com>
2024-03-22 10:14:45 -0500

    print the role

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -13,20 +13,25 @@ const prisma = new PrismaClient({ adapter });
 
 export default async function Home() {
   const user = await currentUser();
-  // This should happen at the middleware layer I think
-  // We need to check if a user exists in the database 'User'. If they don't
-  // we should create them.
-  const isInDB: User | null = await prisma.user.findFirst({
+  let role = null;
+
+  // We first check if the user is in our database (not Clerk's), since
+  // we need to keep track of that for roles.
+  // TODO: There is probably a better name for this
+  const userObject: User | null = await prisma.user.findFirst({
     where: {
       clerkId: user?.id
     }
   });
 
-  if (isInDB == null) {
-    console.log("Creating");
+  // If the user is not in the database, we create them
+  // TODO: It seems this Home() component is loaded twice, 
+  // because the console.log's come up twice.
+  if (userObject == null) {
     await prisma.user.create({
       data: {
         clerkId: user?.id || "",
+        // Role ID 3 here denotes the 'player' RoleId.
         role: {
           connect: {
             id: 3
@@ -35,15 +40,20 @@ export default async function Home() {
       }
     })
   } else {
+    // TODO: Since Prisma is an ORM, it probably has a fancy function for this
+    // already
+    role = await prisma.role.findFirst({where: {
+      id: userObject.roleId
+    }});
+
     console.log("Already created");
   }
-
-  // Otherwise, we know their role
 
   return (
     <>
       <h1 className="text-2xl font-bold">Hello, {user?.firstName}</h1>
-      <p>Is the user in the DB? {isInDB != null ? 'yes' : 'no'}.</p>
+      <p>Is the user in the DB? {userObject != null ? 'yes' : 'no'}.</p>
+      <p>Their role: {role?.name}</p>
     </>
   );
 }
