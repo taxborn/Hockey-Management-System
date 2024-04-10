7071ec7751ca99bc5b7ba68cc60c2a8cfa1e17c6
Braxton Fair <hello@taxborn.com>
2024-04-09 21:54:28 -0500

    start work on pusher for messages

--- app/home/message/page.tsx
+++ app/home/message/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import Pusher from 'pusher-js';
+import { randomBytes } from 'crypto';
+
+Pusher.logToConsole = true;
+
+const pusher = new Pusher("a36faa8b9371d03f341f" || "", {
+  cluster: process.env.PUSHER_CLUSTER || "us2",
+});
+
 export default function Page() {
-  return <p>Messaging Page</p>;
+  const [messages, setMessages] = useState([] as string[]);
+
+  useEffect(() => {
+    const channel = pusher.subscribe('chat-channel');
+
+    channel.bind('chat', function (data: string) {
+      setMessages([...messages, data])
+    });
+
+    console.log(messages);
+
+    return () => {
+      pusher.unsubscribe('chat-channel');
+    };
+  }, [messages]);
+
+  return (
+    <>
+      <h2>Messages</h2>
+      <ul>
+        {messages.map((message) => (
+          <li key={randomBytes(16).join('')}>{message}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
