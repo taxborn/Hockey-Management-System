944bbe641cffd37b61c40b9ec4df7261b080d69f
Braxton Fair <hello@taxborn.com>
2024-04-11 15:59:59 -0500

    More message stuff

--- app/_components/Messages.tsx
+++ app/_components/Messages.tsx
@@ -25,9 +25,11 @@ export default function Messages({ chats, users }: Props) {
 
   useEffect(() => {
     pusherClient.subscribe("chat-channel");
+    console.log("HERE");
 
     pusherClient.bind("chat", (text: string) => {
       setMessages([...messages, text]);
+      console.log("AHHHHHHHHHHHHHHHHHHHH THIS DID HAPPEN");
     });
 
     return () => {
@@ -37,18 +39,18 @@ export default function Messages({ chats, users }: Props) {
 
   return (
     <>
-      <ul className="bg-white overflow-y-scroll max-h-[60vh] sm:max-h-[75vh] flex flex-col-reverse rounded">
-        {chats.map((chat, index) => (
+      <ul className="bg-white  overflow-y-scroll max-h-[60vh] sm:max-h-[75vh] flex flex-col-reverse rounded">
+        {chats.map((chat) => (
           <li
             key={chat.id}
-            className={`py-1 px-1 ${index % 2 === 1 ? "bg-purple-50" : "bg-purple-100"}`}
+            className={`p-1 odd:bg-purple-50 even:bg-purple-100`}
           >
             <div>
               <span className="font-bold">
                 {users.find((user) => user.id == chat.senderId)!.name}
               </span>{" "}
               <span className="text-xs italic text-gray-600">
-                ({chat.createdAt.toLocaleTimeString()})
+                ({chat.createdAt.toLocaleTimeString()}) (#{chat.id})
               </span>
             </div>
             <p>{chat.message}</p>
@@ -56,8 +58,11 @@ export default function Messages({ chats, users }: Props) {
         ))}
         {messages.map((message, index) => (
           <li
-            key={index}
-            className={`py-1 ${index % 2 === 1 ? "bg-purple-50" : "bg-purple-100"}`}
+            // We need to add the length of the chats array to the key to avoid conflicts
+            // between the messages from the database and the messages from Pusher. 
+            // This is probably not the best solution.
+            key={chats.length + index}
+            className={`p-1`}
           >
             ({users.find((user) => user.clerkId == userId)!.name}){message}
           </li>
