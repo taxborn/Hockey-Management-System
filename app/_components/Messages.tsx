76c80f97aaa1143f616d1f55736843a16bcb780d
Braxton Fair <hello@taxborn.com>
2024-04-12 09:49:40 -0500

    finish chat for now

--- app/_components/Messages.tsx
+++ app/_components/Messages.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { pusherClient } from "@/lib/pusher";
 import { useAuth } from "@clerk/nextjs";
 import { Users } from "@prisma/client";
+import { randomBytes } from "crypto";
 
 type Chat = {
   sender: Users;
@@ -20,18 +21,16 @@ interface Props {
 }
 
 export default function Messages({ chats, users }: Props) {
-  const { userId } = useAuth();
-  const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<[string, string, string][]>([]);
 
   useEffect(() => {
     pusherClient.subscribe(process.env.NEXT_PUBLIC_PUSHER_CHAT_CHANNEL!);
-    console.log("HERE");
 
     pusherClient.bind(
       process.env.NEXT_PUBLIC_PUSHER_CHAT_EVENT!,
-      (text: string) => {
-        setMessages([...messages, text]);
-        console.log("AHHHHHHHHHHHHHHHHHHHH THIS DID HAPPEN");
+      (message: [string, string, string]) => {
+        const [text, name, time] = message;
+        setMessages([...messages, [text, name, time]]);
       },
     );
 
@@ -43,6 +42,26 @@ export default function Messages({ chats, users }: Props) {
   return (
     <>
       <ul className="bg-white  overflow-y-scroll max-h-[60vh] sm:max-h-[75vh] flex flex-col-reverse rounded">
+        {messages.reverse().map((message, index) => (
+          <li
+            // We need to add the length of the chats array to the key to avoid conflicts
+            // between the messages from the database and the messages from Pusher.
+            // This is probably not the best solution.
+            key={chats.length + index}
+            className={`p-1 odd:bg-purple-50 even:bg-purple-100`}
+          >
+            <div>
+              <span className="font-bold">
+                {message[1]}
+              </span>{" "}
+              <span className="text-xs italic text-gray-600">
+                ({message[2]}) (#{chats.length + index + 1})
+              </span>
+            </div>
+            <p>{message[0]}</p>
+          </li>
+        ))}
+
         {chats.map((chat) => (
           <li
             key={chat.id}
@@ -59,17 +78,7 @@ export default function Messages({ chats, users }: Props) {
             <p>{chat.message}</p>
           </li>
         ))}
-        {messages.map((message, index) => (
-          <li
-            // We need to add the length of the chats array to the key to avoid conflicts
-            // between the messages from the database and the messages from Pusher.
-            // This is probably not the best solution.
-            key={chats.length + index}
-            className={`p-1`}
-          >
-            ({users.find((user) => user.clerkId == userId)!.name}){message}
-          </li>
-        ))}
+
       </ul>
     </>
   );
