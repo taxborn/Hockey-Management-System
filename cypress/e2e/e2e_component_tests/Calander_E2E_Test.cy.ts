91ee80348190b88313af1fc8ee71f997da53d29d
hermon1738 <117963478+hermon1738@users.noreply.github.com>
2024-04-17 10:31:41 -0500

    calendar test

---
+++ cypress/e2e/e2e_component_tests/Calander_E2E_Test.cy.ts
@@ -0,0 +1,19 @@
+describe('Navigation', () => {
+    it('should navigate to the about page', () => {
+      // Start from the index page
+      cy.visit('http://localhost:3000/', {failOnStatusCode: false})
+   
+      // Find a link with an href attribute containing "sign-in" and click it
+      cy.get('a[href*="/sign-in"]').click()
+      // Type the login credentials 
+      cy.get('input[type*="email"]').type("test@example.com{enter}")
+      cy.get('input[type*="password"]').type("password{enter}")
+      
+      cy.get('button[class *= "cl-formButtonPrimary"]').click()
+      // Wait for the login to finish running 
+      cy.wait(1200)
+      // The new url should include "/home"
+      
+      
+    })
+  })
